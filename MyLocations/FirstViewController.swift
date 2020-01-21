6b531d7d73c08fd4bb3e28d9085cd2f331ed3f75
tomaszosk <t.oskroba@icloud.com>
2020-01-21 21:00:11 +0100

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Tomasz Oskroba on 1/21/20.
+//  Copyright © 2020 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Tomasz Oskroba on 1/21/20.
+//  Copyright © 2020 Tomasz Oskroba. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
